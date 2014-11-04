34bd3628a68365538cb0ba76c0127a5d5bf61c70
Pavel Bastov <pbastov@gmail.com>
2014-11-04 17:07:07 +0600

    Add EndpointFragment

--- app/src/main/java/com/opencbs/androidclient/MainActivity.java
+++ app/src/main/java/com/opencbs/androidclient/MainActivity.java
@@ -1,6 +1,7 @@
 package com.opencbs.androidclient;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EndpointFragment fragment = new EndpointFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.mainFrameLayout, fragment);
+        transaction.commit();
     }
 
 
