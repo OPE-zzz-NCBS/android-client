91d1019db255eb713770ab1ddff24450e65c511f
Pavel Bastov <pbastov@gmail.com>
2014-12-02 12:38:47 +0600

    Adjust the theme
    and style programmatically searchview.

--- app/src/main/java/com/opencbs/androidclient/ui/MainActivity.java
+++ app/src/main/java/com/opencbs/androidclient/ui/MainActivity.java
@@ -3,16 +3,24 @@ package com.opencbs.androidclient.ui;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.opencbs.androidclient.R;
 import com.opencbs.androidclient.Settings;
@@ -22,6 +30,9 @@ import com.opencbs.androidclient.event.LogoutSuccessEvent;
 import com.opencbs.androidclient.event.NewPersonEvent;
 import com.opencbs.androidclient.event.SearchEvent;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import javax.inject.Inject;
@@ -129,6 +140,7 @@ public class MainActivity extends ActivityWithBus implements ListView.OnItemClic
     private void setupSearchMenuItem(Menu menu) {
         final MenuItem searchItem = menu.findItem(R.id.action_search);
         final SearchView searchView = (SearchView) searchItem.getActionView();
+        styleSearchView(searchView);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -156,6 +168,50 @@ public class MainActivity extends ActivityWithBus implements ListView.OnItemClic
                 return true;
             }
         });
+    }
+
+    private void styleSearchView(SearchView searchView) {
+        // It is pretty straightforward to have a light theme with dark actionbar
+        // and light drop down menus. But the search view remains dark and we have to
+        // customize it. This is not a trivial thing to do.
+        // Check out these link for a better picture:
+        // http://www.jayway.com/2014/06/02/android-theming-the-actionbar/
+        // http://novoda.com/blog/styling-actionbar-searchview
+        // http://nlopez.io/how-to-style-the-actionbar-searchview-programmatically/
+
+        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        EditText searchPlate = (EditText) searchView.findViewById(searchPlateId);
+        searchPlate.setTextColor(Color.WHITE);
+        searchPlate.setHintTextColor(Color.LTGRAY);
+        try {
+            Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+            f.setAccessible(true);
+            f.set(searchPlate, 0);
+        } catch (Exception ignored) {
+        }
+
+        try {
+            Class<?> clazz = Class.forName("android.widget.SearchView$SearchAutoComplete");
+            Drawable searchIcon = getResources().getDrawable(R.drawable.ic_action_search);
+            Method textSizeMethod = clazz.getMethod("getTextSize");
+            Float rawTextSize = (Float) textSizeMethod.invoke(searchPlate);
+            int textSize = (int) (rawTextSize*1.25);
+            searchIcon.setBounds(0, 0, textSize, textSize);
+
+            SpannableStringBuilder stopHint = new SpannableStringBuilder("   ");
+            stopHint.setSpan(new ImageSpan(searchIcon), 1, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            Method setHintMethod = clazz.getMethod("setHint", CharSequence.class);
+            setHintMethod.invoke(searchPlate, stopHint);
+        } catch (ClassNotFoundException ignored) {
+        } catch (NoSuchMethodException ignored) {
+        } catch (IllegalAccessException ignored) {
+        } catch (InvocationTargetException ignored) {
+        }
+
+        int closeIconId = searchView.getContext().getResources().getIdentifier("android:id/search_close_btn", null, null);
+        ImageView closeIcon = (ImageView) searchView.findViewById(closeIconId);
+        closeIcon.setImageResource(R.drawable.ic_action_cancel);
     }
 
     private void logout() {
